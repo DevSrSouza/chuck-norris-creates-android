21871d25a293b017979b3f44df44007ac6fe1485
DevSrSouza <devsrsouza@gmail.com>
2020-12-01 01:11:27 -0300

    CI: Removing SQLDelight from the buildscript classpath because of a issue and the project, currently, is not using it

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,5 +14,4 @@ dependencies {
     implementation("com.android.tools.build:gradle:4.2.0-alpha16")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
-    implementation("com.squareup.sqldelight:gradle-plugin:1.4.3")
 }
